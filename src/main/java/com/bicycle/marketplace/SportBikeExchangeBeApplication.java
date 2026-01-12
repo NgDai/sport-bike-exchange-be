dfb72bcdf3834e6a1bd52bfc232418bd530dc555
Yuki <162554358+NgDai@users.noreply.github.com>
2026-01-12 10:35:33 +0700

    Commit: Adding First Github repository

---
+++ src/main/java/com/bicycle/marketplace/SportBikeExchangeBeApplication.java
@@ -0,0 +1,14 @@
+package com.bicycle.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportBikeExchangeBeApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");;
+	}
+
+}
---
+++ src/test/java/com/bicycle/marketplace/SportBikeExchangeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bicycle.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportBikeExchangeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
