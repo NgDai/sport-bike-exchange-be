34912ac17952d8d86e868ffdd42d46c2b3a73f76
Yuki <162554358+NgDai@users.noreply.github.com>
2026-01-17 19:24:10 +0700

    Adding code to User

--- src/main/java/com/bicycle/marketplace/entity/User.java
+++ src/main/java/com/bicycle/marketplace/entity/User.java
@@ -1,5 +1,41 @@
 package com.bicycle.marketplace.entity;
 
-public class User {
-    
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+@AllArgsConstructor
+
+public class User implements Serializable {
+    private String user_id;
+    private String username;
+    private String password;
+    private String fullname;
+    private String phone;
+    private String address;
+    private String date_of_birth;
+    private String id_card;
+    private boolean is_active;
+    private String role;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String user_id, String username, String password, String fullname, String phone, String address, String date_of_birth, String id_card, boolean is_active, String role, String email) {
+        this.user_id = user_id;
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.phone = phone;
+        this.address = address;
+        this.date_of_birth = date_of_birth;
+        this.id_card = id_card;
+        this.is_active = is_active;
+        this.role = role;
+        this.email = email;
+    }
+
+
 }
