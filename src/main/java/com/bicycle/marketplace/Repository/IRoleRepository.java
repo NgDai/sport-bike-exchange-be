3741c45d7747465ef61660fa60aa173ae7e68d96
NgDai <nguyendai2412005@gmail.com>
2026-02-09 16:25:31 +0700

    Remove

--- src/main/java/com/bicycle/marketplace/Repository/IRoleRepository.java
+++
@@ -1,9 +0,0 @@
-/* This file was added during an earlier edit but the user requested a rollback.
-   Keeping this file as a harmless placeholder comment to avoid compile issues if present in VCS.
-   You can safely delete this file if desired.
-*/
-package com.bicycle.marketplace.Repository;
-
-// placeholder - intentionally left blank after rollback
-public interface IRoleRepository {
-}
