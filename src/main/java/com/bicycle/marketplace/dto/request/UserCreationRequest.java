f4bcc56a7304badb258bc69a01a66f759daa59cb
Ngọc Hữu <43675952+votranngochuu@users.noreply.github.com>
2026-03-01 22:20:50 +0700

    validation username cho UserCreation

--- src/main/java/com/bicycle/marketplace/dto/request/UserCreationRequest.java
+++ src/main/java/com/bicycle/marketplace/dto/request/UserCreationRequest.java
@@ -16,8 +16,8 @@ import lombok.Setter;
 public class UserCreationRequest {
 
     @NotBlank(message = "USERNAME_REQUIRED")
-    @Size(min = 3, max = 50, message = "USERNAME_INVALID_LENGTH")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9_-]+$", message = "USERNAME_INVALID_FORMAT")
+    @Size(min = 6, max = 50, message = "USERNAME_INVALID_LENGTH")
+    @Pattern(regexp = "^[a-zA-Z]{6,15}[0-9]*$", message = "USERNAME_INVALID_FORMAT")
     private String username;
 
     @NotBlank(message = "PASSWORD_REQUIRED")
