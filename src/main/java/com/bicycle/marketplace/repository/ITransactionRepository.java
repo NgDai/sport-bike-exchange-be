f92bb5fa14da8ab01904c5adea34a235e1f9eb90
DungKiller <tandungho220105@gmail.com>
2026-03-01 15:30:23 +0700

    done function list by status of Transaction

--- src/main/java/com/bicycle/marketplace/repository/ITransactionRepository.java
+++ src/main/java/com/bicycle/marketplace/repository/ITransactionRepository.java
@@ -4,6 +4,9 @@ import com.bicycle.marketplace.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ITransactionRepository extends JpaRepository<Transaction, Integer> {
+    List<Transaction> findAllByStatus(String status);
 }
