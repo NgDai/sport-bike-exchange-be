87cc0989cd983eee53f882171bfdf1a6406aabb0
NgDai <nguyendai2412005@gmail.com>
2026-03-02 23:11:46 +0700

    VNPay

---
+++ src/main/java/com/bicycle/marketplace/controller/PaymentController.java
@@ -0,0 +1,39 @@
+package com.bicycle.marketplace.controller;
+
+import com.bicycle.marketplace.dto.request.VNPayRequest;
+import com.bicycle.marketplace.dto.response.VNPayResponse;
+import com.bicycle.marketplace.services.VNPayService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+
+    @Autowired
+    private VNPayService vnPayService;
+
+    @PostMapping("/submitOrder")
+    public VNPayResponse submitOrder(@RequestBody VNPayRequest request, HttpServletRequest httpRequest){
+        String baseUrl = httpRequest.getScheme() + "://" + httpRequest.getServerName() + ":" + httpRequest.getServerPort();
+        String vnpayUrl = vnPayService.createOrder(request.getAmount(), request.getOrderInfo(), baseUrl);
+        VNPayResponse response = new VNPayResponse();
+        response.setRedirectUrl(vnpayUrl);
+        return response;
+    }
+
+    @GetMapping("/vnpay-payment")
+    public VNPayResponse handleVnPayReturn(HttpServletRequest request){
+        int paymentStatus = vnPayService.orderReturn(request);
+
+        VNPayResponse response = new VNPayResponse();
+        response.setPaymentStatus(paymentStatus);
+        response.setOrderInfo(request.getParameter("vnp_OrderInfo"));
+        response.setPaymentTime(request.getParameter("vnp_PayDate"));
+        response.setTransactionId(request.getParameter("vnp_TransactionNo"));
+        response.setTotalPrice(request.getParameter("vnp_Amount"));
+
+        return response;
+    }
+}
---
+++ src/main/java/com/bicycle/marketplace/dto/request/VNPayRequest.java
@@ -0,0 +1,18 @@
+package com.bicycle.marketplace.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+public class VNPayRequest {
+    private int amount;
+    private String orderInfo;
+}
+
