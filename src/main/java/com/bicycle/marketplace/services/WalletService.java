3c473230a7ee74c5b957d84ee00d313853aada59
NgDai <nguyendai2412005@gmail.com>
2026-03-05 17:17:35 +0700

    login google

---
+++ src/main/java/com/bicycle/marketplace/services/WalletService.java
@@ -0,0 +1,14 @@
+package com.bicycle.marketplace.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class WalletService {
+
+}
