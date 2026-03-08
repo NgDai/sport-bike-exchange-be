6b4ba87319586695ea5bce5fb464b2a1a2c165d7
Ngọc Hữu <43675952+votranngochuu@users.noreply.github.com>
2026-03-08 10:54:38 +0700

    buildQuery

--- src/main/java/com/bicycle/marketplace/services/VNPayService.java
+++ src/main/java/com/bicycle/marketplace/services/VNPayService.java
@@ -94,66 +94,48 @@ public class VNPayService {
         return (baseUrl != null ? baseUrl.replaceAll("/+$", "") : "") + VNPayConfig.vnp_ReturnUrl;
     }
 
-        cld.add(Calendar.MINUTE, 15);
-        String vnp_ExpireDate = formatter.format(cld.getTime());
-        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+    private String buildQueryWithSecureHash(
+            Map<String, String> params,
+            String encodingCharset
+    ) {
+        List<String> sortedKeys = new ArrayList<>(params.keySet());
+        Collections.sort(sortedKeys);
 
-        List fieldNames = new ArrayList(vnp_Params.keySet());
-        Collections.sort(fieldNames);
-        StringBuilder hashData = new StringBuilder();
+        StringBuilder hashInput = new StringBuilder();
         StringBuilder query = new StringBuilder();
-        Iterator itr = fieldNames.iterator();
-        while (itr.hasNext()) {
-            String fieldName = (String) itr.next();
-            String fieldValue = (String) vnp_Params.get(fieldName);
-            if ((fieldValue != null) && (fieldValue.length() > 0)) {
-                //Build hash data
-                hashData.append(fieldName);
-                hashData.append('=');
-                try {
-                    hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
-                    //Build query
-                    query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
-                    query.append('=');
-                    query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-                if (itr.hasNext()) {
-                    query.append('&');
-                    hashData.append('&');
-                }
+        for (int i = 0; i < sortedKeys.size(); i++) {
+            String key = sortedKeys.get(i);
+            String value = params.get(key);
+            if (value == null) {
+                value = "";
+            }
+            if (hashInput.length() > 0) {
+                hashInput.append('&');
+                query.append('&');
+            }
+            try {
+                String encKey = URLEncoder.encode(key, encodingCharset);
+                String encValue = URLEncoder.encode(value, encodingCharset);
+                hashInput.append(encKey).append('=').append(encValue);
+                query.append(encKey).append('=').append(encValue);
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException("VNPay encoding error", e);
             }
         }
-        String queryUrl = query.toString();
-        String vnp_SecureHash = VNPayConfig.hmacSHA512(VNPayConfig.secretKey, hashData.toString());
-        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
-        String paymentUrl = VNPayConfig.vnp_PayUrl + "?" + queryUrl;
-        return paymentUrl;
+        String secureHash = VNPayConfig.hmacSHA512(
+                VNPayConfig.secretKey,
+                hashInput.toString()
+        );
+        return query + "&vnp_SecureHash=" + secureHash;
     }
 
-    public int orderReturn(HttpServletRequest request){
-        Map fields = new HashMap();
-        for (Enumeration params = request.getParameterNames(); params.hasMoreElements();) {
-            String fieldName = null;
-            String fieldValue = null;
-            try {
-                fieldName = URLEncoder.encode((String) params.nextElement(), StandardCharsets.US_ASCII.toString());
-                fieldValue = URLEncoder.encode(request.getParameter(fieldName), StandardCharsets.US_ASCII.toString());
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-            if ((fieldValue != null) && (fieldValue.length() > 0)) {
-                fields.put(fieldName, fieldValue);
-            }
-        }
-
-        String vnp_SecureHash = request.getParameter("vnp_SecureHash");
-        if (fields.containsKey("vnp_SecureHashType")) {
-            fields.remove("vnp_SecureHashType");
-        }
-        if (fields.containsKey("vnp_SecureHash")) {
-            fields.remove("vnp_SecureHash");
+    private Map<String, String> collectReturnParams(HttpServletRequest request) {
+        Map<String, String> params = new HashMap<>();
+        Enumeration<String> names = request.getParameterNames();
+        while (names.hasMoreElements()) {
+            String name = names.nextElement();
+            String value = request.getParameter(name);
+            params.put(name, value != null ? value : "");
         }
         String signValue = VNPayConfig.hashAllFields(fields);
         if (signValue.equals(vnp_SecureHash)) {
