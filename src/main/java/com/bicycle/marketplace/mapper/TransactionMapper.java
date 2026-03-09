baba79e8f563675047032202e4ffc62dcf66991f
Ngọc Hữu <43675952+votranngochuu@users.noreply.github.com>
2026-03-09 23:41:24 +0700

    mapstruc

--- src/main/java/com/bicycle/marketplace/mapper/TransactionMapper.java
+++ src/main/java/com/bicycle/marketplace/mapper/TransactionMapper.java
@@ -3,12 +3,4 @@ package com.bicycle.marketplace.mapper;
 import com.bicycle.marketplace.dto.request.TransactionUpdateRequest;
 import com.bicycle.marketplace.dto.response.TransactionResponse;
 import com.bicycle.marketplace.entities.Transaction;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-
-@Mapper(componentModel = "spring")
-public interface TransactionMapper {
-    TransactionResponse toTransactionResponse(Transaction transaction);
-    Transaction toTransaction(TransactionCreationRequest request);
-    void updateTransaction(@MappingTarget Transaction transaction, TransactionUpdateRequest request);
 }
