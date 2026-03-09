eae7d33867fa1b2f8faa8fcbc64e12847df8d697
Ngọc Hữu <43675952+votranngochuu@users.noreply.github.com>
2026-03-09 23:36:20 +0700

    import creation request

--- src/main/java/com/bicycle/marketplace/dto/request/TransactionCreationRequest.java
+++ src/main/java/com/bicycle/marketplace/dto/request/TransactionCreationRequest.java
@@ -1,12 +1,10 @@
 package com.bicycle.marketplace.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-
-import java.util.Date;
 
 @Getter
 @Setter
