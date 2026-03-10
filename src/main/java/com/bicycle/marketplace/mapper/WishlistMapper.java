b2c9e4f8ddfa9a9e34b4369835686f9fac1f0df6
DungKiller <tandungho220105@gmail.com>
2026-03-10 21:41:36 +0700

    correct wishlist

--- src/main/java/com/bicycle/marketplace/mapper/WishlistMapper.java
+++ src/main/java/com/bicycle/marketplace/mapper/WishlistMapper.java
@@ -4,8 +4,11 @@ import com.bicycle.marketplace.dto.response.WishlistResponse;
 import com.bicycle.marketplace.dto.response.WishlistToggleResponse;
 import com.bicycle.marketplace.entities.Wishlist;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface WishlistMapper {
+    @Mapping(source = "user.userId", target = "userId")
+    @Mapping(source = "listing.listingId", target = "listingId")
     WishlistResponse toWishlistResponse(Wishlist wishlist);
 }
