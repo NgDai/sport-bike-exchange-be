d855cbbfea981f3107b76269343f9857d4a940ce
DungKiller <tandungho220105@gmail.com>
2026-03-10 15:27:43 +0700

    done get info of Reservation and add function change status

--- src/main/java/com/bicycle/marketplace/mapper/ReservationMapper.java
+++ src/main/java/com/bicycle/marketplace/mapper/ReservationMapper.java
@@ -5,11 +5,17 @@ import com.bicycle.marketplace.dto.request.ReservationUpdateRequest;
 import com.bicycle.marketplace.dto.response.ReservationResponse;
 import com.bicycle.marketplace.entities.Reservation;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ReservationMapper {
+    @Mapping(source = "buyer.userId", target = "buyerId")
+    @Mapping(source = "listing.listingId", target = "listingId")
     ReservationResponse toReservationResponse(Reservation reservation);
+
+    @Mapping(target = "buyer", ignore = true)
+    @Mapping(target = "listing", ignore = true)
     Reservation toReservation(ReservationCreationRequest request);
     void updateReservation(@MappingTarget Reservation reservation, ReservationUpdateRequest request);
 }
