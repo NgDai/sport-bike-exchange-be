cdcec5f73b2df1cd76f44056fc53b42b406ca371
congkha <sinhnhatkha@gmail.com>
2026-03-13 13:09:48 +0700

    update WishlistMapper

--- src/main/java/com/bicycle/marketplace/mapper/WishlistMapper.java
+++ src/main/java/com/bicycle/marketplace/mapper/WishlistMapper.java
@@ -1,7 +1,6 @@
 package com.bicycle.marketplace.mapper;
 
 import com.bicycle.marketplace.dto.response.WishlistResponse;
-import com.bicycle.marketplace.dto.response.WishlistToggleResponse;
 import com.bicycle.marketplace.entities.Wishlist;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,5 +9,12 @@ import org.mapstruct.Mapping;
 public interface WishlistMapper {
     @Mapping(source = "user.userId", target = "userId")
     @Mapping(source = "listing.listingId", target = "listingId")
+    @Mapping(source = "listing.title", target = "listingTitle")
+    @Mapping(source = "listing.description", target = "listingDescription")
+    @Mapping(source = "listing.image_url", target = "listingImageUrl")
+    @Mapping(source = "listing.price", target = "listingPrice")
+    @Mapping(source = "listing.condition", target = "listingCondition")
+    @Mapping(source = "listing.status", target = "listingStatus")
+    @Mapping(source = "listing.seller.fullName", target = "sellerName")
     WishlistResponse toWishlistResponse(Wishlist wishlist);
 }
