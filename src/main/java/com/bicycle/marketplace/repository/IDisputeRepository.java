1fbd7fd9fc514c86fbfb897c7661c6facf2d4d54
Ngọc Hữu <43675952+votranngochuu@users.noreply.github.com>
2026-03-13 21:39:33 +0700

    DisputeRepo

--- src/main/java/com/bicycle/marketplace/repository/IDisputeRepository.java
+++ src/main/java/com/bicycle/marketplace/repository/IDisputeRepository.java
@@ -1,6 +1,7 @@
 package com.bicycle.marketplace.repository;
 
 import com.bicycle.marketplace.entities.Dispute;
+import com.bicycle.marketplace.enums.DisputeStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,14 @@ import java.util.List;
 
 @Repository
 public interface IDisputeRepository extends JpaRepository<Dispute, Integer> {
-    List<Dispute> findAllByStatus(String status);
+
+    List<Dispute> findAllByStatus(DisputeStatus status);
+
+    List<Dispute> findByAssignedInspector_UserId(int inspectorUserId);
+
+    List<Dispute> findByRaisedBy_UserId(int userId);
+
+    /** Tranh chấp mà user tham gia (là người tạo, buyer hoặc seller). */
+    List<Dispute> findByRaisedBy_UserIdOrTransaction_Buyer_UserIdOrTransaction_Seller_UserId(
+            int raisedByUserId, int buyerUserId, int sellerUserId);
 }
