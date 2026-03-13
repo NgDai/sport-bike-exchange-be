f65869ecf6a56cf290d84f75789d63274b34deee
Ngọc Hữu <43675952+votranngochuu@users.noreply.github.com>
2026-03-13 21:38:59 +0700

    CreationRequest

--- src/main/java/com/bicycle/marketplace/dto/request/DisputeCreationRequest.java
+++ src/main/java/com/bicycle/marketplace/dto/request/DisputeCreationRequest.java
@@ -1,20 +1,21 @@
 package com.bicycle.marketplace.dto.request;
 
+import com.bicycle.marketplace.enums.DisputeReasonCategory;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
 
-import java.util.Date;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class DisputeCreationRequest {
+    @NotBlank(message = "Lý do tranh chấp không được để trống")
     private String reason;
-    private String status;
-    private Date createAt;
+    @NotNull(message = "Phân loại lý do tranh chấp là bắt buộc")
+    private DisputeReasonCategory reasonCategory;
 }
