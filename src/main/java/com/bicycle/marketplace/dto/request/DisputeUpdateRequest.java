3cd629f420b9f95af5a41df0717ff6eca3bdee71
Ngọc Hữu <43675952+votranngochuu@users.noreply.github.com>
2026-03-13 21:38:32 +0700

    UpdateRequest

--- src/main/java/com/bicycle/marketplace/dto/request/DisputeUpdateRequest.java
+++ src/main/java/com/bicycle/marketplace/dto/request/DisputeUpdateRequest.java
@@ -1,17 +1,18 @@
 package com.bicycle.marketplace.dto.request;
 
+import com.bicycle.marketplace.enums.DisputeReasonCategory;
+import com.bicycle.marketplace.enums.DisputeStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Date;
-
-@Getter @Setter
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class DisputeUpdateRequest {
     private String reason;
-    private String status;
-    private Date createAt;
+    private DisputeStatus status;
+    private DisputeReasonCategory reasonCategory;
 }
