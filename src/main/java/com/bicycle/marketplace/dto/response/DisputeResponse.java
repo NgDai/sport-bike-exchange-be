e1c4861c4a78a6d359a20d5e3818fcfb88e937ad
Ngọc Hữu <43675952+votranngochuu@users.noreply.github.com>
2026-03-13 21:36:26 +0700

    DisputeResponse

--- src/main/java/com/bicycle/marketplace/dto/response/DisputeResponse.java
+++ src/main/java/com/bicycle/marketplace/dto/response/DisputeResponse.java
@@ -1,5 +1,7 @@
 package com.bicycle.marketplace.dto.response;
 
+import com.bicycle.marketplace.enums.DisputeReasonCategory;
+import com.bicycle.marketplace.enums.DisputeStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,14 +9,19 @@ import lombok.Setter;
 
 import java.util.Date;
 
-@Getter @Setter
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class DisputeResponse {
     private int disputeId;
     private int transactionId;
     private String raisedBy;
+    private Integer raisedByUserId;
     private String reason;
-    private String status;
-    private Date createAt;
+    private DisputeStatus status;
+    private DisputeReasonCategory reasonCategory;
+    private Integer assignedInspectorId;
+    private String assignedInspectorName;
+    private Date createdAt;
 }
